182e7ccda54cb7b6d2e95bd30696f36167e8a5f3
Furkan Kirac <fkirac@gmail.com>
2021-02-22 15:15:53 +0300

    Week 1

---
+++ week1-app1.cpp
@@ -0,0 +1,34 @@
+// Lecture contents will be available in the LMS
+// code will be available in https://github.com/furkankirac/cs409-2020-21-spring
+
+// History of C++
+// Core language + Standard Library
+// Standard Library: libstdc++, libc++
+// CMake: is important, not very likable, but nothing better than it right now :(
+
+// Consider CS321 as a weak prerequisite:
+//  if you didn't get CS321, remember that you can always withdraw the course by the middle of the term
+
+// Midterm Exam 1 (50%) End of April
+// A difficult problem to be solved in xx-days time (open-book)
+// Your unique solution is required
+
+// Midterm Exam 2 (50%) Beginning of June
+// A difficult problem to be solved in xx-days time (open-book)
+// Your unique solution is required
+
+// Will use:
+//   Compiler: Clang 11.1+, GCC 10.2+ and C++20 standard.
+//   You can use one of Clang, GCC, MSVC compilers, and any editor of your choice.
+//   However, I would highly suggest you to use Qt Creator if not familiar with C++.
+//   Editor: Qt Creator 4.14+. Why? Lightweight, very nicely supports CMake.
+
+// COMPILER CONFORMANCE: https://en.cppreference.com/w/cpp/compiler_support
+
+#include <iostream> // implemented for instance in libstdc++, or libc++, STL
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
